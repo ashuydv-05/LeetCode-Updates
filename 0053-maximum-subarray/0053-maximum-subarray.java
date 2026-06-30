1c9b013258406bc34be35d236601157c38424979
ashuydv-05 <161949281+ashuydv-05@users.noreply.github.com>
2026-07-01 00:00:31 +0530

    Time: 1 ms (99.96%), Space: 77.2 MB (47.97%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,14 @@
+//bruteforce
+//kadane algo
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currsum=nums[0];
+        int maxsum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            currsum=Math.max(nums[i],currsum+nums[i]);
+            maxsum=Math.max(maxsum,currsum);
+        }
+        return maxsum;
+  
+    }
+}
