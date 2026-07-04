e23cf1ed1c9dddb844e9b6bb03c26fa8e2457d56
ashuydv-05 <161949281+ashuydv-05@users.noreply.github.com>
2026-07-04 16:54:19 +0530

    Time: 13 ms (72.7%), Space: 48.9 MB (12.51%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -3,19 +3,44 @@ class Solution {
         int n = nums.length;
         int ans[] = new int[n];
         Stack<Integer> st = new Stack<>();
-        for(int i = (2*n)-1 ; i>=0;i--){
-            int idx = i%n;
-            while(!st.isEmpty() && st.peek() <= nums[idx]){
+        int i = n-1;
+        while(i>=0){
+            st.push(nums[i]);
+            i--;
+        }
+        for(int j = n-1 ; j>=0;j--){
+            // int idx = i%n;
+            while(!st.isEmpty() && st.peek() <= nums[j]){
                 st.pop();
             }
             if(st.isEmpty()){
-                ans[idx] = -1;
+                ans[j] = -1;
             }
             else{
-                ans[idx] = st.peek();
+                ans[j] = st.peek();
             }
-            st.push(nums[idx]);
+            st.push(nums[j]);
         } 
         return ans;
     } 
     }
+
+
+    // int n = nums.length;
+    //     int ans[] = new int[n];
+    //     Stack<Integer> st = new Stack<>();
+    //     for(int i = (2*n)-1 ; i>=0;i--){
+    //         int idx = i%n;
+    //         while(!st.isEmpty() && st.peek() <= nums[idx]){
+    //             st.pop();
+    //         }
+    //         if(st.isEmpty()){
+    //             ans[idx] = -1;
+    //         }
+    //         else{
+    //             ans[idx] = st.peek();
+    //         }
+    //         st.push(nums[idx]);
+    //     } 
+    //     return ans;
+    // } 
