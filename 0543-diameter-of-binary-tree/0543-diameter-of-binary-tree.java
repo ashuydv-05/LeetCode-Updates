f77022d0b01ec73866187503d391d68553ffd9a9
ashuydv-05 <161949281+ashuydv-05@users.noreply.github.com>
2026-08-03 00:53:15 +0530

    Time: 0 ms (100%), Space: 46.9 MB (74.46%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -15,19 +15,21 @@
  */
 class Solution {
 
+    static int dia;
      public static int level(TreeNode root){
         //base case
         if(root == null) return 0;
-        return 1 + Math.max(level(root.left), level(root.right));
+        int left_level = level(root.left);
+        int right_level = level(root.right);
+        dia = Math.max(dia,left_level+right_level);
+        return 1 + Math.max(left_level, right_level);
     }
 
 
     public int diameterOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
-        int a = level(root.left) + level(root.right);
-        int b = diameterOfBinaryTree(root.left);
-        int c = diameterOfBinaryTree(root.right);
-        return Math.max(a,Math.max(b,c));
-        
+        dia = 0;
+        level(root);
+        return dia;
+
     }
 }
