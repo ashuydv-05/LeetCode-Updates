71848711c615301cf1ada10c116b7c5f2edc4555
ashuydv-05 <161949281+ashuydv-05@users.noreply.github.com>
2026-08-26 12:27:50 +0530

    Time: 0 ms (100%), Space: 43.7 MB (50.95%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,23 +14,45 @@
  * }
  */
 class Solution {
+    public static List<Integer> sol(TreeNode root, int level, List<Integer> ans){
+
+        // List<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+
+        if(ans.size() == level) ans.add(root.val);
+        
+        sol(root.right,level+1,ans);
+        sol(root.left,level+1,ans);
+
+        return ans;
+     }
+
+
+
     public List<Integer> rightSideView(TreeNode root) {
         
-        List<Integer> ans = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        if(root == null) return ans;
-        q.add(root);
-        while(q.size()>0){
-            int size = q.size();
-            for(int i = 0 ; i< size ;i++){
-                TreeNode front = q.remove();
-                if(i == size-1) ans.add(front.val);
+        // List<Integer> ans = new ArrayList<>();
+        // Queue<TreeNode> q = new LinkedList<>();
+        // if(root == null) return ans;
+        // q.add(root);
+        // while(q.size()>0){
+        //     int size = q.size();
+        //     for(int i = 0 ; i< size ;i++){
+        //         TreeNode front = q.remove();
+        //         if(i == size-1) ans.add(front.val);
 
-                if(front.left != null) q.add(front.left);
-                if(front.right != null) q.add(front.right);
-            }
-        }
-        return ans;
+        //         if(front.left != null) q.add(front.left);
+        //         if(front.right != null) q.add(front.right);
+        //     }
+        // }
+        // return ans;
+
+
+        //METHOD 2 DFS
+        List<Integer> ans = new ArrayList<>();
+        return sol(root,0,ans);
+
+
 
 
 
